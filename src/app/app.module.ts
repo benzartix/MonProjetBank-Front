cce3d59c00f86230e355569ac413669a15ce7b2d
Ahmed Benzarti <benzartix@gmail.com>
2018-08-03 16:16:10 +0200

    finale appAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HttpClientModule } from '@angular/common/http';
-
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
+
+//
+import { HttpClientModule } from '@angular/common/http';
+//
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DetailsCompteComponent } from './details-compte/details-compte.component';
+
+const tableRoutage = [
+  { path: 'comptes', component: ContentComponent},
+  { path: '', component: WelcomeComponent},
+  { path: 'comptes/:id', component: DetailsCompteComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    DetailsCompteComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(tableRoutage) // Routage Links
   ],
   providers: [],
   bootstrap: [AppComponent]
